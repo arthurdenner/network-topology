022846723992dc900eac50d5922673afbda8627b
arthurdenner <arthurdenner7@gmail.com>
2018-02-01 01:20:27 -0300

    feat(Creating frontend for the app):

---
+++ app/frontend/src/App.js
@@ -0,0 +1,210 @@
+import React, { Component, Fragment } from 'react';
+import axios from 'axios';
+import './App.css';
+
+const API_URL = 'http://165.227.178.221/api/dictionary';
+
+class App extends Component {
+  state = {
+    description: '',
+    error: null,
+    isFetching: true,
+    isSaving: false,
+    meaning: '',
+    words: [],
+  };
+
+  componentDidMount() {
+    this.getWords();
+  }
+
+  getWords = async () => {
+    const { data: words } = await axios.get(API_URL);
+
+    this.setState({
+      isFetching: false,
+      words,
+    });
+
+    return words;
+  };
+
+  handleChangeText = evt => {
+    const { id, value } = evt.target;
+
+    this.setState({
+      [id]: value,
+    });
+  };
+
+  handleSaveButton = () => {
+    const { _id, meaning, description } = this.state;
+
+    if (!meaning || !description) {
+      this.setState({
+        error: 'Description and meaning are required!',
+      });
+    } else {
+      this.setState({
+        error: null,
+      });
+
+      this.saveWord({ _id, meaning, description });
+    }
+  };
+
+  handleEdit = evt => {
+    const { id } = evt.target;
+    const { words } = this.state;
+
+    const selectedWord = words.find(w => w._id === id);
+
+    this.setState(selectedWord);
+
+    this.input.focus();
+  };
+
+  handleCancel = () => {
+    this.setState({
+      _id: null,
+      description: '',
+      meaning: '',
+    });
+
+    this.input.focus();
+  };
+
+  handleCloseError = () => {
+    this.setState({
+      error: null,
+    });
+  };
+
+  saveWord = async values => {
+    this.setState({
+      isSaving: true,
+    });
+
+    console.log(values);
+
+    if (!values._id) {
+      await axios.post(`${API_URL}/add`, values);
+    } else {
+      await axios.put(`${API_URL}/${values._id}`);
+    }
+
+    const words = await this.getWords();
+
+    this.setState({
+      description: '',
+      isSaving: false,
+      meaning: '',
+      words,
+    });
+
+    this.input.focus();
+  };
+
+  deleteWord = async evt => {
+    const { id } = evt.target;
+
+    await axios.delete(`${API_URL}/${id}`);
+
+    const words = await this.getWords();
+
+    this.setState({
+      words,
+    });
+  };
+
+  render() {
+    const {
+      _id,
+      description,
+      error,
+      isFetching,
+      isSaving,
+      meaning,
+      words,
+    } = this.state;
+
+    return (
+      <div className="container">
+        {isFetching ? (
+          <div>Loading...</div>
+        ) : (
+          <Fragment>
+            <div className="form">
+              <input
+                ref={el => (this.input = el)}
+                className="field"
+                id="description"
+                onChange={this.handleChangeText}
+                placeholder="Escreva uma descrição"
+                value={description}
+              />
+              <input
+                className="field"
+                id="meaning"
+                onChange={this.handleChangeText}
+                placeholder="Escreva um significado"
+                value={meaning}
+              />
+              <button
+                className="button primary"
+                onClick={this.handleSaveButton}
+              >
+                {isSaving ? 'Salvando...' : 'Salvar'}
+              </button>
+              {_id && (
+                <button
+                  className="button secondary"
+                  onClick={this.handleCancel}
+                >
+                  Cancelar
+                </button>
+              )}
+            </div>
+            {error && (
+              <div className="error">
+                {error} <span onClick={this.handleCloseError}>✖</span>
+              </div>
+            )}
+            <div>
+              {words.length > 0 ? (
+                words.map(w => (
+                  <div className="word" key={w._id}>
+                    <div className="word-header">
+                      <h1>{w.description}</h1>
+                      <div>
+                        <span
+                          id={w._id}
+                          onClick={this.handleEdit}
+                          className="option"
+                        >
+                          Editar
+                        </span>
+                        <span
+                          id={w._id}
+                          className="option"
+                          onClick={this.deleteWord}
+                        >
+                          Deletar
+                        </span>
+                      </div>
+                    </div>
+                    {w.meaning}
+                  </div>
+                ))
+              ) : (
+                <div>Nenhuma palavra encontrada</div>
+              )}
+            </div>
+          </Fragment>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
